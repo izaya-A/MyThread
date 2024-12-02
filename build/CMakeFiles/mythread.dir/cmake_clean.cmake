eeb8f77c8dd161e3beecbf5665844b79f010d73a
izaya-A <2192743739@qq.com>
2024-12-02 14:16:33 +0800

    #

---
+++ build/CMakeFiles/mythread.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mythread.dir/link.d"
+  "CMakeFiles/mythread.dir/main.cpp.o"
+  "CMakeFiles/mythread.dir/main.cpp.o.d"
+  "mythread"
+  "mythread.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mythread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mythread.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for mythread.
+# This may be replaced when dependencies are built.
